d1ab8185ad6d7a984022c70b78c20979a2fe90e9
Loulouw <louzwa@live.fr>
2017-11-28 11:24:18 +0100

    ok

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Salle de Sport</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include 'header.php'; ?>
+
+<div id="headerwrap">
+    <div class="container">
+        <div class="row centered">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h1>It Doesn't Take a Rocket <b>Scientist</b></h1>
+                <h2>It Takes a Designer</h2>
+            </div>
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- headerwrap -->
+
+<div class="w container">
+    <div class="row centered">
+        <br><br>
+        <div class="col-lg-4">
+            <i class="fa fa-heart"></i>
+            <h4>DESIGN</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even believable.</p>
+        </div><!-- col-lg-4 -->
+
+        <div class="col-lg-4">
+            <i class="fa fa-laptop"></i>
+            <h4>BOOTSTRAP</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even believable.</p>
+        </div><!-- col-lg-4 -->
+
+        <div class="col-lg-4">
+            <i class="fa fa-trophy"></i>
+            <h4>SUPPORT</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even believable.</p>
+        </div><!-- col-lg-4 -->
+    </div><!-- row -->
+    <br>
+    <br>
+</div><!-- container -->
+<?php include 'footer.php'; ?>
